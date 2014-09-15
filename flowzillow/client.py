8a4591532b6e4ef236826f75f32df87f6e7c0de7
hahnicity <grehm87@gmail.com>
2014-09-15 00:42:54 -0700

    Got the search client to a place where it works... woohoo

--- flowzillow/client.py
+++ flowzillow/client.py
@@ -1,10 +1,9 @@
 from urlparse import urljoin
-from xml.etree import ElementTree
 
 import requests
 
-from pyzillow import constants
-from pyzillow.exceptions import ZillowError
+from flowzillow import constants
+from flowzillow.exceptions import ZillowError
 
 
 def _trim_none_values(dict_):
@@ -17,6 +16,7 @@ def _trim_none_values(dict_):
         del new_dict[key]
     return new_dict
 
+
 def _validate_response(response):
     if response.status_code != constants.SUCCESS_CODE:
         raise ZillowError(response)
@@ -26,59 +26,92 @@ class SearchClient(object):
     def search(self, latlong1, latlong2, **kwargs):
         """
         Search for all home listings in within a set of rectangular geocoordinates. Returns
-        a block of JSON with all the search results
+        a block of JSON with all the search results. Since this is an undocumented API not
+        all parameters available to this API are of known purpose
 
-        :param tuple latlong1: Tuple of latitude and longitude of the ? rectangular search box
-        :param tuple latlong2: Tuple of latitude and longitude of the ? rectangular search box
+        :param tuple latlong1: Geocoords of the upper left point of the rectangular search box
+        :param tuple latlong2: Geocoords of the lower right point of the rectangular search box
         :param **kwargs:
-        :param rid: Region ID. A unique number per region Oakland CA seems to be 13072. Berkeley CA is 16992
+        :param spt: Seems to be "homes" by default
+        :param rid: Region ID. A region unique number
         :param days: Number of days on market. Select "any" for any number of days
         :param att: Custom keyword search.
         :param sort: Sort by choice of (days/featured/priced/pricea/lot/built/size/baths/beds/zest/zesta)
-        :param zoom: The zoom of the map. It seems like if you set it too low or too high nothing is returned. 12 seems good
+        :param zoom: The zoom of the map.
         :param pf: Search for properties in pre-foreclosure (0/1)
         :param pmf: Search for foreclosed properties (0/1)
-        :param laundry: In unit laundry (rentals only)
-        :param parking: On site-parking (rentals only)
-        :param pets: Accepts pets (rentals only)
-        :param bd: Bedrooms (number plus) eg input of 1 means 1 bedroom and up
+        :param laundry: In unit laundry (rentals only) (0/1)
+        :param parking: On site-parking (rentals only) (0/1)
+        :param pets: Accepts pets (rentals only) (0/1)
+        :param bd: Bedrooms (number plus) eg input of "1," means 1 bedroom and up
         :param pr: Price (number plus) eg input of 50000 means 50000 and up
         :param ba: Bathrooms (number plus)
-        :param sf: Square feet <min>,<max>. If either min or max not set just leave blank but keep comma
-        :param lot: Lot size <min>,<max>
-        :param yr: Year build <min>,<max>
+        :param sf: Square feet "<min>,<max>". If either min or max not set just leave blank but keep comma
+        :param lot: Lot size "<min>,<max>"
+        :param yr: Year build "<min>,<max>"
         :param lt: List Type. A 6 digit binary number for filtering by for sale 111111 would mean search for
         all for sale "By Agent", "By Owner", "Foreclosures", "New Homes", "Open Houses Only", "Coming Soon."
         :param status: Status of home. A 6 digit binary number. input of 111011 means search for all houses
-        (Set to 1 to search "For Sale"), "Make me move", "Recently Sold",
-        (Next bit seems unused), "For Rent", (Set to 1 if you want to search for foreclosure properties)
-        :param ht: Home Type. A 6 digit binary number. 111111 means search for "Houses", "Condos", "Apartments",
-        "Manufactured", "Lots/Land", "Townhomes"
+        (Set to 1 to search "For Sale"), "Make me move", "Recently Sold", (Next bit seems unused),
+        "For Rent", (Set to 1 if you want to search for foreclosure properties)
+        :param ht: Home Type. A 6 digit binary number. 111111 means search for "Houses", "Condos",
+        "Apartments", "Manufactured", "Lots/Land", "Townhomes"
         :param rt: ?? 6 seems to be default
         :param red: ?? 0 seems to be default
+        :param pho: ?? 0 seems to be default
+        :param pnd: ?? 0 seems to be default
+        :param zso: ?? 0 seems to be default
+        :param ds: ?? "all" seems to be default
+        :param p: ?? 1 seems to be default
         """
         params = self._make_search_params(latlong1, latlong2, **kwargs)
         response = requests.get(
-            urljoin(constants.BASE_URL, "search/GetResults.htm"), params=param_dict.items()
+            urljoin(constants.BASE_URL, "search/GetResults.htm"), params=params
         )
         _validate_response(response)
+        import pdb
+        pdb.set_trace()
         return response.json()
 
-    def _make_rect_param(latlong1, latlong2):
-        latlong1 = map(lambda coord: str(coord).replace(".", ""), reversed(latlong1))
-        latlong2 = map(lambda coord: str(coord).replace(".", ""), reversed(latlong2))
-        return ",".join(latlong1 + latlong2)
+    def _make_rect_param(self, latlong1, latlong2):
+        geo1 = map(lambda coord: str(coord).replace(".", ""), reversed(list(latlong1)))
+        geo2 = map(lambda coord: str(coord).replace(".", ""), reversed(list(latlong2)))
+        return ",".join(geo1 + geo2)
 
     def _make_search_params(self, latlong1, latlong2, **kwargs):
-        rect = _make_rect_param(latlong1, latlong2)
-        param_dict.update(rect=rect)
-        param_dict.update(rid=rid)
-        param_dict.update(isMapSearch=False)
-        param_dict.update(search=constants.SEARCH)
-        param_dict.update(red=constants.RED)
-        param_dict.update(rt=constants.RT)
-        param_dict.update(zoom=constants.ZOOM)
-        param_dict = dict(kwargs)
+        rect = self._make_rect_param(latlong1, latlong2)
+        param_dict = {
+            "ht": constants.HOME_TYPE,
+            "isMapSearch": False,
+            "lt": constants.LISTING_TYPE,
+            "rect": rect,
+            "red": constants.RED,
+            "rt": constants.RT,
+            "search": constants.SEARCH,
+            "spt": constants.SPT,
+            "status": constants.STATUS,
+            "zoom": constants.ZOOM_LEVEL,
+            "pr": ",",
+            "mp": ",",
+            "bd": "0,",
+            "ba": "0,",
+            "sf": "0,",
+            "lot": "0,",
+            "yr": "0,",
+            "pho": "0,",
+            "pets": 0,
+            "parking": 0,
+            "laundry": 0,
+            "pnd": 0,
+            "zso": 0,
+            "days": constants.DAYS,
+            "ds": constants.DS,
+            "pf": constants.PF,
+            "pmf": constants.PMF,
+            "p": constants.P,
+            "sort": constants.SORT,
+        }
+        param_dict.update(kwargs)
         return param_dict.items()
 
 
--- flowzillow/constants.py
+++ flowzillow/constants.py
@@ -1,9 +1,19 @@
 from urlparse import urljoin
 
 BASE_URL = "http://zillow.com/"
+DAYS = "any"
+DS = "all"
+HOME_TYPE = 111111
+LISTING_TYPE = 111101
+P = 1
+PF = 1
+PMF = 1
 RED = 0
 RT = 6
 SEARCH = "maplist"
+SORT = "priced"
+SPT = "homes"
+STATUS = 111011
 SUCCESS_CODE = 200
 ZILLOW_WEBSERVICE = urljoin(BASE_URL, "webservice/")
 ZOOM_LEVEL = 12
