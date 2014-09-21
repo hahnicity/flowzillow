9c40a9ed0c4a4c7e159d9caf0ede034b222e0ebb
greg <greg@MinasArnor.(none)>
2014-09-21 15:20:32 -0700

    0.1.1 release

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-__version__ = "0.1"
+__version__ = "0.1.1"
 
 
 setup(
     name="flowzillow",
     author="Gregory Rehm",
     version=__version__,
-    description="An intuitive python wrapper around the Zillow API",
+    description=(
+        "An intuitive python wrapper around the Zillow API. Includes support for the "
+        "undocumented search API"
+    ),
     packages=find_packages(),
     package_data={"*": ["*.html"]},
     install_requires=[
