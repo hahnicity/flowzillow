db6c6d2509e5e85a2e7d22d541534b19c0f14e17
Gregory Rehm <greg@MinasArnor.(none)>
2014-09-21 18:42:51 -0700

    Initial commit. Things are likely to be pretty buggy

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+__version__ = "0.1"
+
+
+setup(
+    name="pyzillow",
+    author="Gregory Rehm",
+    version=__version__,
+    description="An intuitive python wrapper around the Zillow API",
+    packages=find_packages(),
+    package_data={"*": ["*.html"]},
+    install_requires=[
+        "requests"
+    ],
+)
